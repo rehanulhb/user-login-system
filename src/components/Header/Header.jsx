6fe6b5f44bd09abef47983e517028a6ac83d5c2f
rehanulhb <rehanulh@gmail.com>
2024-12-04 20:13:15 +0100

    Designed Register Page and Login Page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
     <li><NavLink to="/">Home</NavLink></li>
     <li><NavLink to="/login">Login</NavLink></li>
     <li><NavLink to="/register">Register</NavLink></li>
+    <li><NavLink to="/heroregister">Hero Register</NavLink></li>
     </>
 
     return (
